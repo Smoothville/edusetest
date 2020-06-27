6dabe44c1d5be175cecd7446af36453d49b5a369
Smoothville <Smoothville@users.noreply.github.com>
2020-06-27 14:18:58 +0100

    Update NewTest2.java

--- src/test/java/com/edu/NewTest2.java
+++ src/test/java/com/edu/NewTest2.java
@@ -7,19 +7,23 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;//add
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 
 
 public class NewTest2 {
 	WebDriver driver;
-
+        // Add options to Google Chrome. The window-size is important for responsive sites
+        ChromeOptions options = new ChromeOptions(); //add
+        options.addArguments("headless"); //add
+        //options.addArguments("window-size=1200x600");
 
   @BeforeMethod
   public void launch() {
 	  
 	    System.setProperty("webdriver.chrome.driver", "chromedriver");
-	    driver = new ChromeDriver();
+	    driver = new ChromeDriver(options); //add options
 	    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 	    driver.get("https://facebook.com");
 
