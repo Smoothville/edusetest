88d3d7c884d859e0d10d53a5ac90365ea360e64d
Smoothville <Smoothville@users.noreply.github.com>
2020-07-11 01:28:50 +0100

    Update NewTest3.java

--- src/test/java/com/edu/NewTest3.java
+++ src/test/java/com/edu/NewTest3.java
@@ -30,54 +30,28 @@ public class NewTest3 {
 		driver.get("192.168.42.9:8081");
   }
 	
-	
-  @Test
-  public void verifyurl() {
-	  
-	    String x = driver.getCurrentUrl();
-	    System.out.println(x);
-	    Assert.assertEquals(x, "http://192.168.42.9:8081/");
-  }
   
-//   @Test
-//   public void verifytitle() {
-	  
-//         String a = driver.getTitle();
-//         System.out.println(a);
-//         //Assert.assertEquals(a, "Facebook");
-// 	//System.out.println("Test Successful.");    
-//   }
+  @Test
+  public void verifyAbout() {
+	  	
+    driver.findElement(By.id("About Us")).click();
+     driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+      WebElement TxtBoxContent = driver.findElement(By.id("PID-ab2-pg"));
+      String result = TxtBoxContent.getText();
+     System.out.println("Printing " + result);
+   System.out.println(result);
+     Assert.assertEquals(result, result);
+	  System.out.println("Test Successful.");
+//  if(driver.getPageSource().contains("This is about page. Lorem Ipsum")){
+// System.out.println("Test Successful.");
+// }else{
+// System.out.println("Test Unsuccessful.");		
+	}
+  }
   
   @AfterMethod
-  public void close() {
-	  
+  public void afterMethod() {  
 	  driver.close();
   }
-}
-	
-	
-  
-//   @Test
-//   public void verifyAbout() {
-	  	
-//     driver.findElement(By.id("About Us")).click();
-//      driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-//       WebElement TxtBoxContent = driver.findElement(By.id("PID-ab2-pg"));
-//       String result = TxtBoxContent.getText();
-//      System.out.println("Printing " + result);
-//    System.out.println(result);
-//      Assert.assertEquals(result, result);
-// 	  System.out.println("Test Successful.");
-// //  if(driver.getPageSource().contains("This is about page. Lorem Ipsum")){
-// // System.out.println("Test Successful.");
-// // }else{
-// // System.out.println("Test Unsuccessful.");		
-// 	}
-//   }
-  
-//   @AfterMethod
-//   public void afterMethod() {  
-// 	  driver.close();
-//   }
 
-// }
+}
